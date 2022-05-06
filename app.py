85d678f33490907b479e4f290d36d6ad4415c049
Electronic12 <sowgatfilm@gmail.com>
2022-05-06 20:03:47 +0500

    just test

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # Call to html file
 @app.route('/')
 def index():
-    first_name = 'Mergen'
+    first_name = 'Kerwen'
     # second_name = '<strong>Abdullayew</strong>'
     second_name = 'Abdullayew'
     favorite_pissa = ['Corekli doner', 'Lawashly doner', 'palow usti', 'Iskender']
@@ -23,11 +23,11 @@ def index():
                             favorite_pissa=favorite_pissa
                             )
 
+
 # localhost:5000/user/jora/26
 @app.route('/user/<string:name>/<int:age>')
 def user(name, age):
     return render_template("user.html", user_name = name, user_age = age)
-
 
 # Create Custom Error Pages
 # Invalid URL
