23dfaeb166fdf5e0544e38b01490dd7306af1347
Electronic12 <sowgatfilm@gmail.com>
2022-05-07 17:54:27 +0500

    bootstrap, flask_wft, wtforms learn

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+from xmlrpc.client import Boolean
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #  Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretversion'
+
+# Create  a Form Class
+class NamerForm(FlaskForm):
+    name = StringField('What is your name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# BooleanField, DateField, DateTimeField, DecimalField, FileField, HiddenField, MultipleField, FieldList,
+# FloatField, FormField, IntegerField, PasswordField, RadioField, SelectField, SubmitField, StringField, TextAreaField
+
+# Validators
+# DataRequired, Email, EqualTo, InputRequired, IPAddress, Length, MacAddress, NumberRagnge, 
+# Optional, Regexp, URL, UUID, AnyOf, NoneOf
+
 
 # Create a routes decorator
 # @app.route('/')
@@ -25,7 +43,7 @@ def index():
 
 
 # localhost:5000/user/jora/26
-@app.route('/user/<string:name>/<int:age>')
+@app.route('/user/<string:name>/<int:age>/')
 def user(name, age):
     return render_template("user.html", user_name = name, user_age = age)
 
@@ -39,6 +57,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route('/name/', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html",
+                            name=name, 
+                            form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
