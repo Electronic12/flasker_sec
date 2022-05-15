46ab6600e83396c199d2a05d5fdd89569e9d74a4
Electronic12 <sowgatfilm@gmail.com>
2022-05-15 07:51:13 +0500

    json file and password compare

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from optparse import check_choice
+import re
 from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, date
 from werkzeug.security import generate_password_hash, check_password_hash
 
 #  Create a Flask Instance
@@ -17,6 +19,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///codemy.db'
 
 db = SQLAlchemy(app)
 
+@app.route('/date/')
+def date_for_json():
+    favorite_games = {
+        "Jora": "Futbol",
+        "Kerwen": "Woleybol",
+        "Mergen": "Judo"
+    }
+    return favorite_games
+    # return {"Date": date.today() }
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,7 +58,15 @@ class UserForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     favorite_color = StringField('Favorite Color')
+    password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Password Must Match')])
+    password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class PasswordForm(FlaskForm):
+    email = StringField('What is Your Email', validators=[DataRequired()])
+    password = PasswordField('What is Your Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/update/<int:id>/', methods=['GET', 'POST'])
 def update(id):
@@ -112,13 +131,15 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
+            hashed_pw = generate_password_hash(form.password_hash.data, 'sha256')
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data, password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
-        form.email.favorite_color = ''
+        form.favorite_color.data = ''
+        form.password_hash.data = ''
         flash('User adedd Successfuly') 
     our_users = Users.query.order_by(Users.date_added)         
     return render_template("add_user.html", name=name, form=form, our_users=our_users)
@@ -154,6 +175,28 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route('/test_pw/', methods=['GET', 'POST'])
+def test_pw():
+    email = None
+    password = None
+    pw_to_check = None
+    passed = None
+    form = PasswordForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        form.email.data = ''
+        form.password.data = ''
+        pw_to_check = Users.query.filter_by(email=email).first()
+        passed = check_password_hash(pw_to_check.password_hash, password)
+
+    return render_template("test_pw.html",
+                            email=email,
+                            password=password, 
+                            pw_to_check = pw_to_check,
+                            passed =passed,
+                            form=form)
 
 @app.route('/name/', methods=['GET', 'POST'])
 def name():
